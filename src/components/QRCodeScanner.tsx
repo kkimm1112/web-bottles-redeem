4ac757b6f6c51a7f33f8e9504f1a489543cb177b
kkimm1112 <kim.kajoompoo@gmail.com>
2025-05-03 03:26:31 +0700

    1467

--- src/components/QRCodeScanner.tsx
+++ src/components/QRCodeScanner.tsx
@@ -355,15 +355,6 @@ export default function QRCodeScannerWithPoints({ onScanSuccess }: { onScanSucce
           <button className="rescan-button" onClick={handleRescan}>
             สแกนใหม่
           </button>
-          <button 
-                  onClick={() => window.location.href = "/"}
-                  className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200 font-medium flex items-center justify-center"
-                >
-                  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"></path>
-                  </svg>
-                  ดำเนินการต่อ
-                </button>
         </div>
       )}
       <style jsx>{`
