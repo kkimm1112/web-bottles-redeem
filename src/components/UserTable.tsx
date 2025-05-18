7e406650509ea5aacac0da8a7c0d7193efc8d696
bass5068 <148568963+bass5068@users.noreply.github.com>
2025-05-18 22:24:55 +0700

    fix10

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -264,17 +264,6 @@ export default function UserTable() {
       )}
       
       {/* Add User Button */}
-      <div className="mt-4 flex justify-end">
-        <button 
-          className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-          onClick={() => router.push('/admin/users/new')}
-        >
-          <svg className="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
-          </svg>
-          Add User
-        </button>
-      </div>
     </div>
   );
 }
